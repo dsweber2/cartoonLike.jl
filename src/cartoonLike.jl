50eef8d66be6a0415b7a84757f883d1b7b2e8700
dsweber2 <david.weber2@gmail.com>
2019-07-19 15:06:29 -0700

    curvature examples

--- src/cartoonLike.jl
+++ src/cartoonLike.jl
@@ -5,9 +5,25 @@ using LinearAlgebra
 using Interpolations
 using NearestNeighbors
 
-export create_circle, given_points
+export create_circle, given_points, distinguishing_curvature, cartoonLikeImage
+
+
+
+
 # still to make: piecewise smooth curves
 #                fill in the interior, exterior with something smooth
+
+struct cartoonLikeImage{T}
+  xSize::Int
+  ySize::Int
+  ninterp::Int
+  ε::T
+  function cartoonLikeImage{T}(; xSize=100, ySize=100, ninterp = xSize*ySize,
+                               ε = 1/(xSize*ySize)^(3/4)) where T<: Real
+    return new(xSize, ySize, ninterp, T(ε))
+  end
+end
+
 
 """
     create_circle(r, rate = 10)
@@ -22,14 +38,19 @@ end
 
 
 """
-    image, xlocs, ylocs = given_points(points; xSize=100, ySize=100, ninterp = xSize*ySize, ε = .01)
+    image, xlocs, ylocs = given_points(points; xSize=100, ySize=100, ninterp =
+        xSize*ySize, ε = .01)
 
 given a collection of nodes, i.e. row vectors of the form (x,y), make a
 (xSize)×(ySize) image of path going through them, which has been blurred with a
 gaussian of standard deviation of ε (\epsilon)
 """
-function given_points(points; xSize=100, ySize=100, ninterp = xSize*ySize, ε =
-                      1/(xSize*ySize)^(3/4))
+function given_points(points::Array{T}; cart::cartoonLikeImage{<:Float64} =
+                      cartoonLikeImage{T}()) where T<:Real
+    xSize = cart.xSize
+    ySize = cart.ySize
+    ninterp = cart.ninterp
+    ε = cart.ε
     t= range(0, 1, length = size(points, 1))
     itp = scale(interpolate(points, (BSpline(Cubic(Natural(OnGrid()))), NoInterp())), t, 1:2)
     tfine = range(0, 1, length = ceil(Int, ninterp))
@@ -38,7 +59,7 @@ function given_points(points; xSize=100, ySize=100, ninterp = xSize*ySize, ε =
 
     xRange = range(-1, 1, length=xSize)
     yRange = range(-1, 1, length=ySize)
-
+    
     imagePoints = zeros(2, xSize, ySize)
     for (ix,x) =enumerate(xRange), (iy,y) = enumerate(yRange)
         imagePoints[1, ix, iy] = x
@@ -47,20 +68,40 @@ function given_points(points; xSize=100, ySize=100, ninterp = xSize*ySize, ε =
     image = zeros(xSize, ySize)
     idxs, dists = knn(tree, reshape(imagePoints,(2,:)),1)
     dists = [d[1] for d in dists]       # the distances are given as lists, and we want
-                                        # it as just an array
+    # it as just an array
     dists = reshape(dists, size(image))
     image = exp.(-(dists).^2 ./ε)
     return (image, xRange, yRange)
 end
+
+"""
+make a collection of `n` images that have a range of different curvatures at a
+  central point, equally spaced
+"""
+function distinguishing_curvature(n; cart::cartoonLikeImage{<:Float64} =
+                                  cartoonLikeImage()) 
+  yRange = range(-1,1,length=2*n+1)
+  points = make_points.(yRange)
+  image_groups = given_points.(make_points.(yRange), cart =
+                               cartoonLikeImage{Float64}())
+  xLocs = image_groups[1][2]
+
+    yLocs = image_groups[1][3]
+
+  images = zeros(cart.xSize, cart.ySize, length(image_groups))
+  for (i,tup) in enumerate(image_groups)
+    images[:, :, i] = tup[1]
+  end
 end
 
-points = create_circle(.4)
-image, xLoc, yLoc = given_points(points, ninterp=100*100,ε=.001)
-heatmap(xLoc, yLoc, image)
 
 
-points = [-.5  -.9   0.0 .25   .5 .75;
-          -.75 -.125 .9  .5  -.25 -.75]'
-image, xLoc, yLoc = given_points(points, xSize=samples, ySize=samples)
-heatmap(xLoc, yLoc, image')
+function make_points(y)
+  return [-.5 0 .5;
+           y  0  y]'
+end
 
+
+
+
+end
