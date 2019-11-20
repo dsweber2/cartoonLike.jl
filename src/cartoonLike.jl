39229438ea569efc16bc680d5c17fdbd4608ce52
dsweber2 <david.weber2@gmail.com>
2019-11-20 12:17:26 -0800

    not just float64, that's dumb

--- src/cartoonLike.jl
+++ src/cartoonLike.jl
@@ -45,7 +45,7 @@ given a collection of nodes, i.e. row vectors of the form (x,y), make a
 (xSize)×(ySize) image of path going through them, which has been blurred with a
 gaussian of standard deviation of ε (\epsilon)
 """
-function given_points(points::Array{T}; cart::cartoonLikeImage{<:Float64} =
+function given_points(points::AbstractArray{T}; cart::cartoonLikeImage{<:Real} =
                       cartoonLikeImage{T}()) where T<:Real
     xSize = cart.xSize
     ySize = cart.ySize
@@ -78,20 +78,20 @@ end
 make a collection of `n` images that have a range of different curvatures at a
   central point, equally spaced
 """
-function distinguishing_curvature(n; cart::cartoonLikeImage{<:Float64} =
-                                  cartoonLikeImage()) 
-  yRange = range(-1,1,length=2*n+1)
-  points = make_points.(yRange)
-  image_groups = given_points.(make_points.(yRange), cart =
-                               cartoonLikeImage{Float64}())
-  xLocs = image_groups[1][2]
+function distinguishing_curvature(nPoints; cart::cartoonLikeImage{<:Real} =
+                                  cartoonLikeImage{Float32}()) 
+    yRange = range(-1,1,length=2*nPoints+1)
+    points = make_points.(yRange)
+    image_groups = given_points.(points, cart = cart)
+    xLocs = image_groups[1][2]
 
     yLocs = image_groups[1][3]
 
-  images = zeros(cart.xSize, cart.ySize, length(image_groups))
-  for (i,tup) in enumerate(image_groups)
-    images[:, :, i] = tup[1]
-  end
+    images = zeros(cart.xSize, cart.ySize, length(image_groups))
+    for (i,tup) in enumerate(image_groups)
+        images[:, :, i] = tup[1]
+    end
+    return images
 end
 
 
